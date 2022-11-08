06cafd567867b929977bed0daa684064dede19b9
Parth <parth@halalbox.in>
2022-11-08 12:29:47 +0530

    Added function to create payment url.

--- api/getStripePaymentUrl.js
+++ api/getStripePaymentUrl.js
@@ -59,5 +59,5 @@ const paymentUrl = session.url;
   response.status(200).json({paymentUrl});
 }
 
-// export default allowCors(getStripePaymentUrl);
-export default getStripePaymentUrl;  
+export default allowCors(getStripePaymentUrl);
+// export default getStripePaymentUrl;  
